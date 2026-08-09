b99914b2a57441240bda8f57c90702b5c9d15303
Sak1na-IT <pasayevasekine844@gmail.com>
2026-08-09 11:39:46 +0000

    python

---
+++ challenge/4-delete_dnodeint/print_dlistint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+    dlistint_t *current;
+    unsigned int i;
+
+    if (head == NULL || *head == NULL)
+        return (-1);
+
+    current = *head;
+    i = 0;
+
+    while (current != NULL && i < index)
+    {
+        current = current->next;
+        i++;
+    }
+
+    if (current == NULL)
+        return (-1);
+
+    if (current->prev != NULL)
+        current->prev->next = current->next;
+    else
+        *head = current->next;
+
+    if (current->next != NULL)
+        current->next->prev = current->prev;
+
+    free(current);
+
+    return (1);
+}
